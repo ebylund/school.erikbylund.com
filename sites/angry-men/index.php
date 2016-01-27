a9f492dc65964b658d2c27b53f6441a1ad5cee70
Erik Bylund <ebylundini@gmail.com>
2016-01-27 00:43:00 -0700

    finish navbar

--- sites/angry-men/index.php
+++ sites/angry-men/index.php
@@ -3,8 +3,35 @@
 <head>
 	<meta charset="UTF-8">
 	<title>12 Angry Men</title>
+	<link rel="stylesheet" href="css/style.css" media="screen" title="no title" charset="utf-8">
 </head>
 <body>
-	this is just so angry
+
+	<div class="container">
+
+		<div class="jurorNav">
+			<div id="juror1" class="juror"></div>
+			<div id="juror2" class="juror"></div>
+			<div id="juror3" class="juror"></div>
+			<div id="juror4" class="juror"></div>
+			<div id="juror5" class="juror"></div>
+			<div id="juror6" class="juror"></div>
+		</div>
+
+		<nav class="clear">
+			<div class="navMenu">
+				<ul>
+					<li class="navMenuItem"><a href="">HOME</a></li>
+					<li class="navMenuItem"><a href="">CAST</a></li>
+					<li class="navMenuItem"><a href="">STORY</a></li>
+					<li class="navMenuItem"><a href="">IMAGES</a></li>
+					<li class="navMenuItem"><a href="">TRAILER</a></li>
+					<li class="navMenuItem"><a href="">DOWNLOAD</a></li>
+				</ul>
+			</div>
+		</nav>
+
+	</div>
+
 </body>
 </html>
