07efd8499d66f99bd133b199334cfd460e156b38
Erik Bylund <ebylundini@gmail.com>
2016-01-28 00:51:56 -0700

    finish home page

--- sites/angry-men/index.php
+++ sites/angry-men/index.php
@@ -7,20 +7,19 @@
 </head>
 <body>
 
-		<nav class="clear">
-			<div class="navMenu">
-				<ul>
-					<li class="navMenuItem"><a href="">HOME</a></li>
-					<li class="navMenuItem"><a href="">CAST</a></li>
-					<li class="navMenuItem"><a href="">STORY</a></li>
-					<li class="navMenuItem"><a href="">IMAGES</a></li>
-					<li class="navMenuItem"><a href="">TRAILER</a></li>
-					<li class="navMenuItem"><a href="">DOWNLOAD</a></li>
-				</ul>
-			</div>
-		</nav>
+	<nav class="clear">
+		<div class="navMenu">
+			<ul>
+				<li class="navMenuItem"><a href="">HOME</a></li>
+				<li class="navMenuItem"><a href="">CAST</a></li>
+				<li class="navMenuItem"><a href="">STORY</a></li>
+				<li class="navMenuItem"><a href="">IMAGES</a></li>
+				<li class="navMenuItem"><a href="">TRAILER</a></li>
+				<li class="navMenuItem"><a href="">DOWNLOAD</a></li>
+			</ul>
+		</div>
+	</nav>
 	<div class="container">
-
 		<div class="jurorNav">
 			<div class="jurorBox"><img src="img/outline01.png" id="juror1" class="juror" /></div>
 			<div class="jurorBox"><img src="img/outline02.png" id="juror1" class="juror" /></div>
@@ -29,11 +28,15 @@
 			<div class="jurorBox"><img src="img/outline05.png" id="juror5" class="juror" /></div>
 			<div class="jurorBox"><img src="img/outline06.png" id="juror6" class="juror" /></div>
 		</div>
-
+		<div class="centerPiece">
+			<img src="img/12AngryMenStag.svg" alt="12 man" class="piece">
+		</div>
+			<div class="centerPiece">
+				<img src="img/quote.svg" alt="12 man" class="piece">
+			</div>
 	</div>
 
 	<div class="Bottom">
-	
 		<div class="container centerIt">
 			<div class="jurorBox"><img src="img/outline07.png" id="juror7" class="juror" /></div>
 			<div class="jurorBox"><img src="img/outline08.png" id="juror8" class="juror" /></div>
