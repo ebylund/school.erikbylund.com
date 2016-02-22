28abe2f2d977bb1b31f4e606c32b216093d598f1
Erik Bylund <ebylundini@gmail.com>
2016-02-22 03:39:09 -0700

    working site

--- mobile/js/main.js
+++ mobile/js/main.js
@@ -11,6 +11,8 @@ $(document).ready(function() {
         $("header").css("height", ($size/7.68)+"vh");
         $(".app_heading > .app_header_container > h2").css("font-size", ($size/450)+"em");
         $(".about_us_content_left > p").css("font-size", ($size/600)+"em");
+        $(".feature_label_wrapper > h1").css("font-size", ($size/307)+"em");
+        $(".feature_label_wrapper > p").css("font-size", ($size/438)+"em");
     };
 
     resizeElems();
