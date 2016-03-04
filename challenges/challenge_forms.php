8f5b044e36dfa7db3cc1ee22dfec0726f645d011
Erik Bylund <ebylundini@gmail.com>
2016-03-04 16:29:30 -0700

    add beginning header section

---
+++ challenges/challenge_forms.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>formy forms</title>
+</head>
+<body>
+
+    <form action="challenge_forms_display.php" method="post">
+        <input type="text" placeholder="hello goop" name="ssn">
+        <input type="submit" name="Hola" value="Hola">
+    </form>
+
+</body>
+</html>
---
+++ challenges/challenge_forms_display.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>posty postum</title>
+</head>
+<body>
+    <?php
+        echo $_POST['ssn'];
+    ?>
+
+</body>
+</html>
