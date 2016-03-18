e2c285a403305ebe1d90a758d0c538dcbcca129a
Erik Bylund <ebylundini@gmail.com>
2016-03-18 00:38:48 -0600

    update mail shizzz

--- challenges/php/submit.php
+++ challenges/php/submit.php
@@ -1,10 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Contact</title>
+    <link rel="stylesheet" href="../css/php_form.css" media="screen" title="no title" charset="utf-8">
+    <link rel="stylesheet" href="../css/contact.css" media="screen" title="no title" charset="utf-8">
+</head>
+<body>
     <?php
         $name = $_POST['name'];
         $email = $_POST['email'];
         $comments = $_POST['comments'];
         $errors = [];
         $welcome = "Thanks for giving up your information so freely, I promise to not make too of a profit from selling it";
-        $wasSent = mail($email, 'whats up player' . $name, $welcome . "\n\nYour comments: ". $comments);
+        $wasSent = mail("ebylundini@gmail.com", "whats up player", $welcome);
     ?>
     <?php if($wasSent): ?>
     <h1>Success!</h1>
@@ -24,3 +33,5 @@
         </form>
         <h6>*** We might sell your info</h6>
     </div> -->
+</body>
+</html>
